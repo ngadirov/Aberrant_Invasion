c2858a68ca752ad9b7cc58ebd9b59c4ba510014e
ngadirov <nezirgadirov@gmail.com>
2022-12-29 00:19:39 +0100

    correccion de errores

--- Assets/Scripts/MC_Controller.cs
+++ Assets/Scripts/MC_Controller.cs
@@ -18,11 +18,14 @@ public class MC_Controller : MonoBehaviour
     public float timeInvincible = 2.0f;
     bool isInvincible;
     float invincibleTimer;
+    bool isMoving = false;
 
+    Animator animator;
 
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
         currentHealth = maxHealth;
     }
 
@@ -31,25 +34,27 @@ public class MC_Controller : MonoBehaviour
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
-
-  //      if(position.x != 0)
-		//{
-  //          lastHorizontaVector = 
-		//}
+        
 
         Vector2 position = rigidbody2d.position;
         position.x = position.x + speed * horizontal * Time.fixedDeltaTime;
         position.y = position.y + speed * vertical *  Time.fixedDeltaTime;
 
-        rigidbody2d.MovePosition(position);
+        animator.SetFloat("Horizontal", horizontal);
+		//animator.SetFloat("Speed", position.x);
 
-        if (position.x != 0)
+
+		rigidbody2d.MovePosition(position);
+
+		if (horizontal != 0)
 		{
-            lastHorizontaVector = position.x;
+            lastHorizontaVector = horizontal;
+            isMoving = true;
 		}
-        if(position.y != 0)
+        if(vertical != 0)
 		{
-            lastVerticalVector = position.y;
+            lastVerticalVector = vertical;
+            isMoving = true;
 		}
 
 
